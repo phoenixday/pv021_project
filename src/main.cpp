1ad1848d4120542bf3c0105e5e7e894dffaa37cf
I587489 <alina.tsykynovska@sap.com>
2023-12-03 16:33:14 +0100

    try

--- src/main.cpp
+++ src/main.cpp
@@ -143,7 +143,8 @@ void writePredictions(const std::string& filename, const std::vector<int>& data)
 int main() {
     cout << "HIDDEN_SIZE1: " << HIDDEN_SIZE1 
         << ", HIDDEN_SIZE2: " << HIDDEN_SIZE2 
-        << ", HIDDEN_SIZE3: " << HIDDEN_SIZE3 << endl;
+        << ", HIDDEN_SIZE3: " << HIDDEN_SIZE3
+        << ", LAMBDA: " << LAMBDA << endl;
 
     vector<vector<double>> train_vectors = readVectors("data/fashion_mnist_train_vectors.csv");
     vector<int> train_labels = readLabels("data/fashion_mnist_train_labels.csv");
