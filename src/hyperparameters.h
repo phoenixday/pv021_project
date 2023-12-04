40cd0207fac455c50ae80abcbb7755d7c030d933
I587489 <alina.tsykynovska@sap.com>
2023-12-04 22:47:37 +0100

    edit

--- src/hyperparameters.h
+++ src/hyperparameters.h
@@ -1,10 +1,10 @@
 #ifndef HYPERPARAMETERS_H
 #define HYPERPARAMETERS_H
 
-const int EPOCHS = 8;
+const int EPOCHS = 4;
 const int INPUT_SIZE = 784;
-const int HIDDEN_SIZE1 = 128;
-const int HIDDEN_SIZE2 = 64;
+const int HIDDEN_SIZE1 = 256;
+const int HIDDEN_SIZE2 = 128;
 const int OUTPUT_SIZE = 10;
 const int BATCH_SIZE = 32;
 const double LEARNING_RATE = 0.001;
