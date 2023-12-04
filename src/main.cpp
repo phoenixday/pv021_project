606dbee529bd443f4cac09a55a91e5e4c6a56c14
I587489 <alina.tsykynovska@sap.com>
2023-12-04 06:38:05 +0100

    tries (not working)

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,41 @@
 
 using namespace std;
 
+class Matrix {
+public:
+    vector<vector<double>> data;
+
+    Matrix(int rows, int cols) : data(rows, vector<double>(cols, 0.0)) {}
+
+    int numRows() const { return data.size(); }
+    int numCols() const { return data[0].size(); }
+
+    vector<double>& operator[](int index) { return data[index]; }
+    const vector<double>& operator[](int index) const { return data[index]; }
+
+    Matrix transpose() const {
+        Matrix result(numCols(), numRows());
+        for (int i = 0; i < numRows(); ++i)
+            for (int j = 0; j < numCols(); ++j)
+                result[j][i] = data[i][j];
+        return result;
+    }
+
+    static Matrix multiply(const Matrix& a, const Matrix& b) {
+        if (a.numCols() != b.numRows()) {
+            cerr << "Dimension mismatch in matrix multiplication" << endl;
+            exit(1); // or handle the error in a more suitable way for your application
+        }
+        
+        Matrix result(a.numRows(), b.numCols());
+        for (int i = 0; i < a.numRows(); ++i)
+            for (int j = 0; j < b.numCols(); ++j)
+                for (int k = 0; k < a.numCols(); ++k)
+                    result[i][j] += a[i][k] * b[k][j];
+        return result;
+    }
+};
+
 // Hyperparameters
 const int EPOCHS = 8;
 const int INPUT_SIZE = 784; // 28x28
@@ -28,75 +63,110 @@ double reluDerivative(double x) {
     return (x > 0) ? 1.0 : 0.0;
 }
 
-void softmax(vector<double> &x) {
-    double max_val = *max_element(x.begin(), x.end());
-    double sum = 0.0;
-    for (auto &val : x) {
-        val = exp(val - max_val);
-        sum += val;
-    }
-    for (auto &val : x) {
-        val /= sum;
-    }
-}
-
-void passHidden(vector<double> &prev_layer, vector<double> &layer,
-                const vector<double> &weights, const vector<double> &bias) {
-    for (long unsigned int i = 0; i < layer.size(); ++i) {
-        for (long unsigned int j = 0; j < prev_layer.size(); ++j) {
-            layer[i] += prev_layer[j] * weights[i * prev_layer.size() + j];
+void softmax(Matrix& mat) {
+    for (int j = 0; j < mat.numCols(); ++j) {
+        double maxVal = mat[0][j];
+        for (int i = 1; i < mat.numRows(); ++i) {
+            if (mat[i][j] > maxVal) {
+                maxVal = mat[i][j];
+            }
         }
-        layer[i] += bias[i];
-        layer[i] = relu(layer[i]);
-    }
-}
 
-void passOutput(vector<double> &prev_layer, vector<double> &output,
-                const vector<double> &weights, const vector<double> &bias) {
-    for (long unsigned int i = 0; i < OUTPUT_SIZE; ++i) {
-        for (long unsigned int j = 0; j < prev_layer.size(); ++j) {
-            output[i] += prev_layer[j] * weights[i * prev_layer.size() + j];
+        double sum = 0.0;
+        for (int i = 0; i < mat.numRows(); ++i) {
+            mat[i][j] = exp(mat[i][j] - maxVal);
+            sum += mat[i][j];
         }
-        output[i] += bias[i];
-    }
-    softmax(output);
-}
 
-void backpropagationHidden(vector<double> &layer, vector<double> &d_layer, 
-                           vector<double> &d_next_layer, vector<double> next_layer_weights) {
-    for (long unsigned int i = 0; i < d_layer.size(); ++i) {
-        double error = 0.0;
-        for (long unsigned int j = 0; j < d_next_layer.size(); ++j) {
-            error += d_next_layer[j] * next_layer_weights[j * d_layer.size() + i];
+        for (int i = 0; i < mat.numRows(); ++i) {
+            mat[i][j] /= sum;
         }
-        d_layer[i] = error * reluDerivative(layer[i]); 
     }
 }
 
-void updateWeightsWithAdam(vector<double> &weights, vector<double> &bias,
-                           const vector<double> &gradients, const vector<double> &inputs,
-                           vector<double> &m_weights, vector<double> &v_weights, int epoch) {
-    int layerSize = bias.size();
-    int inputSize = inputs.size();
-
-    for (int h = 0; h < layerSize; ++h) {
-        for (int j = 0; j < inputSize; ++j) {
-            int idx = h * inputSize + j;
-            m_weights[idx] = BETA1 * m_weights[idx] + (1.0 - BETA1) * inputs[j] * gradients[h];
-            v_weights[idx] = BETA2 * v_weights[idx] + (1.0 - BETA2) * pow(inputs[j] * gradients[h], 2);
-            double m_corr = m_weights[idx] / (1.0 - pow(BETA1, epoch));
-            double v_corr = v_weights[idx] / (1.0 - pow(BETA2, epoch));
-            weights[idx] += LEARNING_RATE * (m_corr / (sqrt(v_corr) + EPSILON) + LAMBDA * weights[idx]);
+void initializeWithDis(Matrix& input, normal_distribution<>& dis, mt19937& gen) {
+    for (int i = 0; i < input.numRows(); ++i) {
+        for (int j = 0; j < input.numCols(); ++j) {
+            input[i][j] = dis(gen);
         }
-        bias[h] += LEARNING_RATE * gradients[h];
     }
 }
 
-vector<vector<double>> readVectors(const string &filename) {
+void resetLayer(Matrix& input) {
+    for (int i = 0; i < input.numRows(); ++i) {
+        for (int j = 0; j < input.numCols(); ++j) {
+            input[i][j] = 0.0;
+        }
+    }
+}
+
+void passHidden(const Matrix& prev_layer, Matrix& layer, const Matrix& weights, const vector<double>& bias) {
+    Matrix transposed_prev_layer = prev_layer.transpose();
+    Matrix product = Matrix::multiply(weights, transposed_prev_layer);
+
+    if (product.numRows() != layer.numRows() || product.numCols() != layer.numCols()) {
+        cerr << "Dimension mismatch in passHidden function" << endl;
+        exit(1);
+    }
+
+    for (int i = 0; i < product.numRows(); ++i) {
+        for (int j = 0; j < product.numCols(); ++j) {
+            layer[i][j] = relu(product[i][j] + bias[i]);
+        }
+    }
+}
+
+void passOutput(const Matrix& layer, Matrix& output, const Matrix& weights, const vector<double>& bias) {
+    Matrix transposed_layer = layer.transpose();
+    Matrix product = Matrix::multiply(weights, transposed_layer);
+
+    for (int i = 0; i < product.numRows(); ++i) {
+        for (int j = 0; j < product.numCols(); ++j) {
+            output[i][j] = product[i][j] + bias[i];
+        }
+    }
+    softmax(output); 
+}
+
+void backpropagationHidden(const Matrix& layer, Matrix& d_layer, const Matrix& d_next_layer, const Matrix& next_layer_weights) {
+    Matrix transposed_weights = next_layer_weights.transpose();
+
+    for (int i = 0; i < d_layer.numRows(); ++i) {
+        for (int j = 0; j < d_layer.numCols(); ++j) {
+            double error = 0.0;
+            for (int k = 0; k < d_next_layer.numRows(); ++k) {
+                error += d_next_layer[k][j] * transposed_weights[i][k];
+            }
+            d_layer[i][j] = error * reluDerivative(layer[i][j]);
+        }
+    }
+}
+
+void updateWeightsWithAdam(Matrix& weights, vector<double>& bias, const Matrix& gradients, const Matrix& inputs, Matrix& m_weights, Matrix& v_weights, int epoch) {
+    for (int i = 0; i < weights.numRows(); ++i) {
+        for (int j = 0; j < weights.numCols(); ++j) {
+            m_weights[i][j] = BETA1 * m_weights[i][j] + (1.0 - BETA1) * gradients[i][j];
+            v_weights[i][j] = BETA2 * v_weights[i][j] + (1.0 - BETA2) * pow(gradients[i][j], 2);
+
+            double m_corr = m_weights[i][j] / (1.0 - pow(BETA1, epoch));
+            double v_corr = v_weights[i][j] / (1.0 - pow(BETA2, epoch));
+
+            weights[i][j] += LEARNING_RATE * (m_corr / (sqrt(v_corr) + EPSILON) + LAMBDA * weights[i][j]);
+        }
+    }
+
+    for (size_t i = 0; i < bias.size(); ++i) {
+        bias[i] += LEARNING_RATE * gradients[i][0];
+    }
+}
+
+
+vector<Matrix> readVectors(const string &filename) {
     cout << "Reading vectors from " << filename << " ..." << endl;
     ifstream file(filename);
     string line;
-    vector<vector<double>> data;
+
+    vector<Matrix> data;
     while (getline(file, line)) {
         stringstream lineStream(line);
         string cell;
@@ -104,8 +174,13 @@ vector<vector<double>> readVectors(const string &filename) {
         while (getline(lineStream, cell, ',')) {
             row.push_back(stod(cell));
         }
-        data.push_back(row);
+        Matrix matrixRow(1, row.size());
+        for (size_t j = 0; j < row.size(); ++j) {
+            matrixRow[0][j] = row[j];
+        }
+        data.push_back(matrixRow);
     }
+
     if (data.empty()) {
         cerr << filename << " is empty or could not be read." << endl;
     } else {
@@ -130,20 +205,20 @@ vector<int> readLabels(const string &filename) {
     return data;
 }
 
-void writePredictions(const std::string& filename, const std::vector<int>& data) {
+void writePredictions(const string& filename, const vector<int>& data) {
     cout << "Writing predictions to " << filename << " ..." << endl;
-    std::ofstream file(filename);
+    ofstream file(filename);
     for (int val : data) {
-        file << val << std::endl;
+        file << val << endl;
     }
     cout << "Predictions written!" << endl;
     file.close();
 }
 
 int main() {
-    vector<vector<double>> train_vectors = readVectors("data/fashion_mnist_train_vectors.csv");
+    vector<Matrix> train_vectors = readVectors("data/fashion_mnist_train_vectors.csv");
     vector<int> train_labels = readLabels("data/fashion_mnist_train_labels.csv");
-    vector<vector<double>> test_vectors = readVectors("data/fashion_mnist_test_vectors.csv");
+    vector<Matrix> test_vectors = readVectors("data/fashion_mnist_test_vectors.csv");
     vector<int> test_labels = readLabels("data/fashion_mnist_test_labels.csv");
 
     // Validation split (last 10% of training data)
@@ -162,14 +237,14 @@ int main() {
     normal_distribution<> dis_output(0, sqrt(2.0 / HIDDEN_SIZE3));
 
     // weights
-    vector<double> hidden_weights1(INPUT_SIZE * HIDDEN_SIZE1);
-    vector<double> hidden_weights2(HIDDEN_SIZE1 * HIDDEN_SIZE2);
-    vector<double> hidden_weights3(HIDDEN_SIZE2 * HIDDEN_SIZE3);
-    vector<double> output_weights(HIDDEN_SIZE3 * OUTPUT_SIZE);
-    for (auto &w : hidden_weights1) w = dis_hidden1(gen);
-    for (auto &w : hidden_weights2) w = dis_hidden2(gen);
-    for (auto &w : hidden_weights3) w = dis_hidden3(gen);
-    for (auto &w : output_weights) w = dis_output(gen);
+    Matrix hidden_weights1(INPUT_SIZE, HIDDEN_SIZE1);
+    Matrix hidden_weights2(HIDDEN_SIZE1, HIDDEN_SIZE2);
+    Matrix hidden_weights3(HIDDEN_SIZE2, HIDDEN_SIZE3);
+    Matrix output_weights(HIDDEN_SIZE3, OUTPUT_SIZE);
+    initializeWithDis(hidden_weights1, dis_hidden1, gen);
+    initializeWithDis(hidden_weights2, dis_hidden2, gen);
+    initializeWithDis(hidden_weights3, dis_hidden3, gen);
+    initializeWithDis(output_weights, dis_output, gen);
 
     // biases
     vector<double> hidden_bias1(HIDDEN_SIZE1);
@@ -182,20 +257,20 @@ int main() {
     for (auto &b : output_bias) b = dis_output(gen);
 
     // Pre-allocate memory for hidden and output vectors
-    vector<double> hidden1(HIDDEN_SIZE1);
-    vector<double> hidden2(HIDDEN_SIZE2);
-    vector<double> hidden3(HIDDEN_SIZE3);
-    vector<double> output(OUTPUT_SIZE);
+    Matrix hidden1(1, HIDDEN_SIZE1);
+    Matrix hidden2(1, HIDDEN_SIZE2);
+    Matrix hidden3(1, HIDDEN_SIZE3);
+    Matrix output(1, OUTPUT_SIZE);
 
     // Initialize Adam weights
-    vector<double> m_hidden_weights1(INPUT_SIZE * HIDDEN_SIZE1, 0.0);
-    vector<double> v_hidden_weights1(INPUT_SIZE * HIDDEN_SIZE1, 0.0);
-    vector<double> m_hidden_weights2(HIDDEN_SIZE1 * HIDDEN_SIZE2, 0.0);
-    vector<double> v_hidden_weights2(HIDDEN_SIZE1 * HIDDEN_SIZE2, 0.0);
-    vector<double> m_hidden_weights3(HIDDEN_SIZE2 * HIDDEN_SIZE3, 0.0);
-    vector<double> v_hidden_weights3(HIDDEN_SIZE2 * HIDDEN_SIZE3, 0.0);
-    vector<double> m_output_weights(HIDDEN_SIZE2 * OUTPUT_SIZE, 0.0);
-    vector<double> v_output_weights(HIDDEN_SIZE2 * OUTPUT_SIZE, 0.0);
+    Matrix m_hidden_weights1(INPUT_SIZE, HIDDEN_SIZE1);
+    Matrix v_hidden_weights1(INPUT_SIZE, HIDDEN_SIZE1);
+    Matrix m_hidden_weights2(HIDDEN_SIZE1, HIDDEN_SIZE2);
+    Matrix v_hidden_weights2(HIDDEN_SIZE1, HIDDEN_SIZE2);
+    Matrix m_hidden_weights3(HIDDEN_SIZE2, HIDDEN_SIZE3);
+    Matrix v_hidden_weights3(HIDDEN_SIZE2, HIDDEN_SIZE3);
+    Matrix m_output_weights(HIDDEN_SIZE2, OUTPUT_SIZE);
+    Matrix v_output_weights(HIDDEN_SIZE2, OUTPUT_SIZE);
 
     vector<int> train_predictions;
 
@@ -212,10 +287,10 @@ int main() {
             int i = indices[idx];
 
             // Reset hidden and output layers
-            fill(hidden1.begin(), hidden1.end(), 0.0);
-            fill(hidden2.begin(), hidden2.end(), 0.0);
-            fill(hidden3.begin(), hidden3.end(), 0.0);
-            fill(output.begin(), output.end(), 0.0);
+            resetLayer(hidden1);
+            resetLayer(hidden2);
+            resetLayer(hidden3);
+            resetLayer(output);
 
             // Forward pass
             passHidden(train_vectors[i], hidden1, hidden_weights1, hidden_bias1);
@@ -223,29 +298,27 @@ int main() {
             passHidden(hidden2, hidden3, hidden_weights3, hidden_bias3);
             passOutput(hidden3, output, output_weights, output_bias);
 
-            // Write to train_predictions.csv
-            int predicted_label = max_element(output.begin(), output.end()) - output.begin();
-            if (epoch == EPOCHS - 1) train_predictions.push_back(predicted_label);
+            // Write 
+            int predicted_label = distance(output[0].begin(), max_element(output[0].begin(), output[0].end()));
+            if (epoch == EPOCHS) train_predictions.push_back(predicted_label);
 
             // Compute the loss and error
-            vector<double> error_output(OUTPUT_SIZE, 0.0);
-            for (int o = 0; o < OUTPUT_SIZE; ++o) {
-                error_output[o] = (o == train_labels[i]) ? 1.0 - output[o] : 0.0 - output[o];
+            Matrix error_output(1, OUTPUT_SIZE);
+            int actual_label = train_labels[idx];
+            for (int i = 0; i < OUTPUT_SIZE; ++i) {
+                error_output[0][i] = (i == actual_label) ? 1.0 - output[0][i] : -output[0][i];
             }
 
             // Backpropagation
-            vector<double> d_output(OUTPUT_SIZE, 0.0);
-            for (int o = 0; o < OUTPUT_SIZE; ++o) {
-                d_output[o] = error_output[o]; 
-            }
+            Matrix d_output = error_output;
 
-            vector<double> d_hidden3(HIDDEN_SIZE3, 0.0);
+            Matrix d_hidden3(1, HIDDEN_SIZE3);
             backpropagationHidden(hidden3, d_hidden3, d_output, output_weights);
 
-            vector<double> d_hidden2(HIDDEN_SIZE2, 0.0);
+            Matrix d_hidden2(1, HIDDEN_SIZE2);
             backpropagationHidden(hidden2, d_hidden2, d_hidden3, hidden_weights3);
 
-            vector<double> d_hidden1(HIDDEN_SIZE1, 0.0);
+            Matrix d_hidden1(1, HIDDEN_SIZE1);
             backpropagationHidden(hidden1, d_hidden1, d_hidden2, hidden_weights2);
 
             updateWeightsWithAdam(hidden_weights1, hidden_bias1, d_hidden1, train_vectors[i],
@@ -265,10 +338,10 @@ int main() {
         int correct_count = 0;
         for (int i = training_size; i < total_training; ++i) {
             // Reset hidden and output layers
-            fill(hidden1.begin(), hidden1.end(), 0.0);
-            fill(hidden2.begin(), hidden2.end(), 0.0);
-            fill(hidden3.begin(), hidden3.end(), 0.0);
-            fill(output.begin(), output.end(), 0.0);
+            resetLayer(hidden1);
+            resetLayer(hidden2);
+            resetLayer(hidden3);
+            resetLayer(output);
 
             // Forward pass
             passHidden(train_vectors[i], hidden1, hidden_weights1, hidden_bias1);
@@ -276,8 +349,8 @@ int main() {
             passHidden(hidden2, hidden3, hidden_weights3, hidden_bias3);
             passOutput(hidden3, output, output_weights, output_bias);
             
-            // Write to train_predictions.csv
-            int predicted_label = max_element(output.begin(), output.end()) - output.begin();
+            // Write
+            int predicted_label = distance(output[0].begin(), max_element(output[0].begin(), output[0].end()));
             if (epoch == EPOCHS) train_predictions.push_back(predicted_label);
             correct_count += (predicted_label == train_labels[i]) ? 1 : 0;
         }
@@ -290,10 +363,10 @@ int main() {
     vector<int> test_predictions;
     for (size_t i = 0; i < test_vectors.size(); ++i) {
         // Reset hidden and output layers
-        fill(hidden1.begin(), hidden1.end(), 0.0);
-        fill(hidden2.begin(), hidden2.end(), 0.0);
-        fill(hidden3.begin(), hidden3.end(), 0.0);
-        fill(output.begin(), output.end(), 0.0);
+        resetLayer(hidden1);
+        resetLayer(hidden2);
+        resetLayer(hidden3);
+        resetLayer(output);
 
         // Forward pass with test vectors
         passHidden(test_vectors[i], hidden1, hidden_weights1, hidden_bias1);
@@ -301,8 +374,8 @@ int main() {
         passHidden(hidden2, hidden3, hidden_weights3, hidden_bias3);
         passOutput(hidden3, output, output_weights, output_bias);
 
-        // Write to test_predictions.csv
-        int predicted_label = max_element(output.begin(), output.end()) - output.begin();
+        // Write
+        int predicted_label = distance(output[0].begin(), max_element(output[0].begin(), output[0].end()));
         test_predictions.push_back(predicted_label);
         correct_count += (predicted_label == test_labels[i]) ? 1 : 0;
     }
